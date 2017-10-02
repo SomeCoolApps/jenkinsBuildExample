597e33313f598f856d57414b63b3fa65e04f80cc
Sunil <sunil.scr@gmail.com>
2017-10-02 13:14:05 -0500

    First commit

---
+++ src/main/java/com/example/demo/JenkinsBuildExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBuildExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsBuildExampleApplication.class, args);
+	}
+}
