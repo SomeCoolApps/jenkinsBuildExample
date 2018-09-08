19c3a3b649a00e45f0ae52df6a589aea6aacc7b4
sunil kumar <sunil.scr@gmail.com>
2018-09-08 11:49:09 -0500

    Update JenkinsBuildExampleApplication.java

--- src/main/java/com/example/demo/JenkinsBuildExampleApplication.java
+++ src/main/java/com/example/demo/JenkinsBuildExampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsBuildExampleApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(JenkinsBuildExampleApplication.class, args);
 	}
 }
